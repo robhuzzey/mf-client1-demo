ddb4a86ca606775ad30d89fe46e1d9c33e39e86c
robhuzzey <github@differentelements.co.uk>
2023-02-17 17:25:52 +0000

    init

---
+++ webpack.config.js
@@ -0,0 +1,63 @@
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
+
+const deps = require("./package.json").dependencies;
+module.exports = (_, argv) => ({
+    output: {
+    publicPath: argv.mode === "development" ? "http://localhost:8080/" : "https://mf-host-demo.vercel.app/",
+  },
+
+    resolve: {
+      extensions: [".jsx", ".js", ".json"],
+  },
+
+    devServer: {
+      port: 8080,
+  },
+
+    module: {
+      rules: [
+        {
+          test: /\.m?js/,
+          type: "javascript/auto",
+          resolve: {
+            fullySpecified: false,
+          },
+        },
+        {
+          test: /\.css$/i,
+          use: ["style-loader", "css-loader"],
+        },
+        {
+          test: /\.(js|jsx)$/,
+          exclude: /node_modules/,
+          use: {
+            loader: "babel-loader",
+          },
+        },
+      ],
+  },
+
+    plugins: [
+      new ModuleFederationPlugin({
+        name: "client1",
+        filename: "remoteEntry.js",
+        remotes: {},
+        exposes: {},
+        shared: {
+          ...deps,
+          react: {
+            singleton: true,
+            requiredVersion: deps.react,
+          },
+          "react-dom": {
+            singleton: true,
+            requiredVersion: deps["react-dom"],
+          },
+        },
+      }),
+      new HtmlWebPackPlugin({
+        template: "./src/index.html",
+      }),
+    ],
+});
